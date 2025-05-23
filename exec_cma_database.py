4ce77ca4513a70b10c39ac29c7d8cfb0cd6391bc
Nate <nate.kitzmiller@indwes.edu>
2025-05-23 10:25:17 -0400

    cleanup options

--- exec_cma_database.py
+++ exec_cma_database.py
@@ -75,18 +75,18 @@ cmaA = True             # Run CMA1 instead of CMA0
 csv = True               # Generate database .csv file
 SI = False                # Generate LaTeX SI file
 #SI = True               # Generate LaTeX SI file
-compute_all = False       # run calculations for all or a select few
-#compute_all = True       # run calculations for all or a select few
+#compute_all = False       # run calculations for all or a select few
+compute_all = True       # run calculations for all or a select few
 # off_diag_bands = False   # (CMA2/3 ONLY) If set to true, "n" off-diag bands selected, if false, "n" largest fc will be selected
                            # off_diag_bands is now an obsolete option
 off_diag = 0   # Set this option for CMA0
 # off_diag = 1   # Set this option for CMA1. Additional off-diagonal elements will need to be specified using ___.
 # off_diag = 2   # Set this option for CMA2. Off-diags will be auto generated, but an aux hessian will need be specified using ___.
 deriv_level = 0         # (CMA1) if 0, compute initial hessian by singlepoints. If 1, compute initial hessian with findif of gradients
-second_order = True    # If True, read in cartesian gradient and force constant info to be converted to internal coordinates.
-#second_order = False    # If False, generate displacements to manually compute the CMA-0A internal coord force constants.
-coord_type_init = "cartesian" # Toggle this for type of coordinate used in inital force constant computations
-#coord_type_init = "internal" # Toggle this for type of coordinate used in inital force constant computations
+#second_order = True    # If True, read in cartesian gradient and force constant info to be converted to internal coordinates.
+second_order = False    # If False, generate displacements to manually compute the CMA-0A internal coord force constants.
+#coord_type_init = "cartesian" # Toggle this for type of coordinate used in inital force constant computations
+coord_type_init = "internal" # Toggle this for type of coordinate used in inital force constant computations
 
 #molsym_symmetry = True
 molsym_symmetry = False
