96217d10ecb771e8cf965ae0c54775109e89b697
Nate <nate.kitzmiller@indwes.edu>
2025-06-18 19:27:26 -0400

    alanine

---
+++ 10_Alanine/1_xab/B3LYP_6-311G**/Disps_gfn1/DispsInit/1/optstep.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=Concordant             # Job name
+#SBATCH --partition=batch               # Partition (queue) name
+#SBATCH --cpus-per-task=4     # Number of cores per MPI rank 
+#SBATCH --mem=40GB        # Memory per processor
+#SBATCH --time=72:00:00
+#SBATCH --output=output.dat
+#SBATCH --hint=compute_bound
+
+source /nfs/cluster_config/modules.sh
+module load xtb/6.7.1
+
+#eval "$(conda shell.bash hook)"
+## Load their personal Conda environment
+#conda activate xtbenv
+#capture the submission directory
+SUBMIT_DIR="$SLURM_SUBMIT_DIR"
+
+#scratch directory
+SCRATCH_DIR="/scratch/$USER/$SLURM_JOB_ID"
+
+srun --cpu-bind=verbose xtb --gfn 1 input.coord --hess --grad
+
+
+echo "Job \$SLURM_JOB_ID running on \$HOSTNAME"
+echo "CPU affinity:"
+taskset -cp $$
